9f8198b8c5dfa89488bb76e87ad32f0a436028b6
sudip <gsudip97+git@gmail.com>
2023-04-22 14:30:20 +0200

    add html and css, js to be worked on

---
+++ index.js
@@ -0,0 +1,39 @@
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import {getDatabase, ref, push, onValue} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings={
+    databaseURL : "https://playground-8b025-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "items")
+
+//console.log(database)
+
+const addButtonEl = document.getElementById("add-button")
+const inputFieldEl = document.getElementById("input-field")
+const shoppingListEl = document.getElementById("shopping-list")
+
+const clearInputFieldEl = ()=>{
+    inputFieldEl.value =""
+}
+
+const appendItemToShoppingListEl=(newValue)=>{
+    shoppingListEl.innerHTML += `<li>${newValue}</li>`
+}
+
+addButtonEl.addEventListener("click", ()=>{
+    let inputValue = inputFieldEl.value
+
+    if (inputValue === ""){
+        return
+    }
+
+    push(shoppingListInDB ,inputValue)
+
+    appendItemToShoppingListEl(inputValue)
+    console.log(shoppingListEl.innerHTML)
+    clearInputFieldEl()
+    console.log(`${inputValue} added to list`)
+})
